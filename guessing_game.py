8fb048c32a481ec5e0c371fb516d381df50ec1b7
Katrina Connors <koktakatrina@gmail.com>
2023-11-27 17:14:58 -0800

    Added various functions.

--- guessing_game.py
+++ guessing_game.py
@@ -1,2 +1,42 @@
+#greet player
+#get player name
+#choose random number between 1 and 100
+#repeat forever:
+ #   get guess
+  #  if guess is incorrect:
+   #     give hint
+    #    increase number of guesses
+    #else:
+     #   congratulate player
+
+import random
+
 print("Hi!")
-name = input("What is you name?")
+name = input("What is your name?")
+
+random_number = random.randint(1,100)
+#print(random_number)
+guess = None
+guess_count = 0
+while guess != random_number:
+    guess = input("What is your guess?")
+    if not("0" in guess or "1" in guess or "2" in guess or "3" in guess or "4" in guess or "5" in guess or "6" in guess or "7" in guess or "8" in guess or "9" in guess):
+        print("Not a valid input. Type a number.")
+        break
+    else:
+        guess = int(guess)
+        guess_count +=1
+    
+    if guess < random_number:
+        print(f"{name}, guess is too low.")
+    elif guess > random_number:
+        print(f"{name}, your guess is too high.")
+    elif guess == random_number:
+        print(f"{name}, your guess is correct! It took you {guess_count} tries.") 
+
+    
+   
+
+
+
+
